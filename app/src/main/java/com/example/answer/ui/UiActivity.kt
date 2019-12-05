8ae83d127235cc4ccb7201b9181003ad02c00944
victory316 <csh153@gmail.com>
2019-12-05 21:16:08 +0900

    adding assets, setting up ui top bar

--- app/src/main/java/com/example/answer/ui/UiActivity.kt
+++ app/src/main/java/com/example/answer/ui/UiActivity.kt
@@ -1,5 +1,7 @@
 package com.example.answer.ui
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.answer.R
@@ -9,5 +11,9 @@ class UiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ui)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = Color.BLACK
+        }
     }
 }
