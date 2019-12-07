a17bfb00f6d99de7dfbc4b1d8759b2998b5cbf2e
victory316 <csh153@gmail.com>
2019-12-07 19:02:35 +0900

    continuing testing on bars

--- app/src/main/java/com/example/answer/ui/recyclerview/ConferenceAdapter.kt
+++ app/src/main/java/com/example/answer/ui/recyclerview/ConferenceAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.answer.ui.recyclerview
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -28,8 +29,43 @@ class ConferenceAdapter(val contactItemClick: (ConferenceRoomData) -> Unit, val 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         private val nameTv = itemView.findViewById<TextView>(R.id.room_name)
         private val numberTv = itemView.findViewById<TextView>(R.id.room_location)
+        private val timeBarArray = listOf(
+            itemView.findViewById<View>(R.id.bar_1),
+            itemView.findViewById<View>(R.id.bar_2),
+            itemView.findViewById<View>(R.id.bar_3),
+            itemView.findViewById<View>(R.id.bar_4),
+            itemView.findViewById<View>(R.id.bar_5),
+            itemView.findViewById<View>(R.id.bar_6),
+            itemView.findViewById<View>(R.id.bar_7),
+            itemView.findViewById<View>(R.id.bar_8),
+            itemView.findViewById<View>(R.id.bar_9),
+            itemView.findViewById<View>(R.id.bar_10),
+            itemView.findViewById<View>(R.id.bar_11),
+            itemView.findViewById<View>(R.id.bar_12),
+            itemView.findViewById<View>(R.id.bar_13),
+            itemView.findViewById<View>(R.id.bar_14),
+            itemView.findViewById<View>(R.id.bar_15),
+            itemView.findViewById<View>(R.id.bar_16),
+            itemView.findViewById<View>(R.id.bar_17),
+            itemView.findViewById<View>(R.id.bar_18)
+        )
 
         fun bind(conferenceRoomData: ConferenceRoomData) {
+
+            Log.d("recyclerViewTest", "name : ${conferenceRoomData.name}")
+
+            val testReservation = conferenceRoomData.reservations
+
+            if (testReservation != null) {
+                for(indices in testReservation) {
+                    Log.d("recyclerViewTest", "starttime : ${indices.startTime}")
+                    Log.d("recyclerViewTest", "endtime : ${indices.endTime}")
+
+                    // 예약시간에 따라 timeline bar의 색상을 지정
+                    setupTimelineBars(indices.startTime, indices.endTime, timeBarArray)
+                }
+            }
+
             nameTv.text = conferenceRoomData.name
             numberTv.text = conferenceRoomData.location
 
@@ -47,6 +83,33 @@ class ConferenceAdapter(val contactItemClick: (ConferenceRoomData) -> Unit, val 
     fun setContacts(contacts: List<ConferenceRoomData>) {
         this.contacts = contacts
         notifyDataSetChanged()
+    }
+
+    fun setupTimelineBars(startTime: String, endTime: String, timeArray: List<View>){
+        val startTimeInt = startTime.toInt()
+        val endTimeInt = endTime.toInt()
+
+        val startHour = startTimeInt / 100
+        val startMinute = startTimeInt % 100
+        val endHour = endTimeInt / 100
+        val endMinute = endTimeInt % 100
+
+        // 9시면 0번 인덱스부터 시작해야하며, 18시가 마지막 19번째 index로 위치함.
+        // 각 칸은 30분 단위로 나뉘어짐을 고려해야 함.
+
+        var startIndex = (startHour - 9) * 2 + (startMinute / 30)
+        var endIndex = (endHour - 9) * 2 + (endMinute / 30)
+
+        if (endIndex >= timeArray.size) endIndex = 17
+
+        Log.d("recyclerViewTest", "parsed time : $startHour : $startMinute | $endHour : $endMinute")
+        Log.d("recyclerViewTest", "startIndex : $startIndex endIndex : $endIndex")
+
+
+        for (i in startIndex..endIndex) {
+            timeArray[i].visibility = View.VISIBLE
+        }
+
     }
 
 }
